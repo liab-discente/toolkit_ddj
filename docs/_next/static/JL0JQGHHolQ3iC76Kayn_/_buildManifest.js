4abd171c9452eb3dbed678a515a39338317af773
Lia Porto <liabarcellos28@gmail.com>
2021-07-27 10:41:29 -0300

    Adicionar dados fictícios e criar cards básicos para teste

--- docs/_next/static/JL0JQGHHolQ3iC76Kayn_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/140-a94f2cc822e21befff0d.js","static/css/eb291d7de33c874d5a88.css","static/chunks/pages/index-70edb20aa6d71ac58972.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
